cb0c22838a54056722c0492148f822f345c2641a
EvanXzj <2686465170@qq.com>
2019-04-30 12:16:55 +0800

    lunch

--- pkg/upload/image.go
+++ pkg/upload/image.go
@@ -1,1 +1,85 @@
 package upload
+
+import (
+	"fmt"
+	"log"
+	"mime/multipart"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/EvanXzj/gin-blog/pkg/logging"
+
+	"github.com/EvanXzj/gin-blog/pkg/file"
+
+	"github.com/EvanXzj/gin-blog/pkg/setting"
+	"github.com/EvanXzj/gin-blog/pkg/util"
+)
+
+// GetImagePath 获取图片路径
+func GetImagePath() string {
+	return setting.AppSetting.ImgageSavePath
+}
+
+// GetImageName 获取图片名称
+func GetImageName(name string) string {
+	ext := path.Ext(name)
+	fileName := strings.TrimSuffix(name, ext)
+	fileName = util.EncodeMD5(fileName)
+
+	return fileName + ext
+}
+
+// GetImageFullUrl 获取图片完整URL
+func GetImageFullUrl(name string) string {
+	return setting.AppSetting.ImagePrefixUrl + "/" + GetImagePath() + name
+}
+
+// GetImageFullPath 获取图片的完整路径
+func GetImageFullPath() string {
+	return setting.AppSetting.RuntimeRootPath + GetImagePath()
+}
+
+// CheckImageExt checks image extension
+func CheckImageExt(filename string) bool {
+	ext := file.GetExt(filename)
+	for _, allowExt := range setting.AppSetting.ImageAllowExts {
+		if strings.ToUpper(allowExt) == strings.ToUpper(ext) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// CheckImageSize checks image size
+func CheckImageSize(f multipart.File) bool {
+	size, err := file.GetSize(f)
+	if err != nil {
+		log.Println(err)
+		logging.Warn(err)
+		return false
+	}
+
+	return size <= setting.AppSetting.ImageMaxSize
+}
+
+// CheckImage checks image state
+func CheckImage(src string) error {
+	dir, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("os.Getwd err: %v", err)
+	}
+
+	err = file.IsNotExistMkdir(dir + "/" + src)
+	if err != nil {
+		return fmt.Errorf("file.IsNotExistMkDir err: %v", err)
+	}
+
+	perm := file.CheckPermission(src)
+	if perm == true {
+		return fmt.Errorf("file.CheckPermission Permission denied src: %s", src)
+	}
+
+	return nil
+}
